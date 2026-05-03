e682ef7d7a2d72bb138acfc5464cd3ae9a3bd98e
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-05-03 15:30:14 +0900

    feat: add nix module

---
+++ modules/nix/default.nix
@@ -0,0 +1,58 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.programs.nix;
+in
+{
+  options.programs.nix = {
+    enable = mkEnableOption "";
+  };
+
+  config = mkIf cfg.enable {
+    nix = {
+      enable = true;
+
+      channel = {
+        enable = false;
+      };
+
+      extraOptions = ''
+        max-silent-time = 3600
+      '';
+
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 7d";
+      };
+
+      optimise = {
+        automatic = true;
+      };
+
+      settings = {
+        auto-optimise-store = pkgs.stdenv.isLinux;
+        experimental-features = [
+          "flakes"
+          "nix-command"
+          "pipe-operators"
+        ];
+        sandbox = if pkgs.stdenv.isDarwin then "relaxed" else true;
+        trusted-users = (
+          [
+            "root"
+            "@wheel"
+          ]
+          ++ optional pkgs.stdenv.isDarwin "@admin"
+        );
+        use-xdg-base-directories = true;
+        warn-dirty = false;
+      };
+    };
+  };
+}
