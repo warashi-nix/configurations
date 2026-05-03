ea0bb6fc96bb15de2c012d0335dc8a7bfeb8e177
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-05-03 14:36:10 +0900

    feat(athena): add initial configuration for athena

---
+++ hosts/darwin/athena/homebrew.nix
@@ -0,0 +1,50 @@
+{
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+    };
+    brews = [
+      "mas"
+    ];
+    casks = [
+      # keep-sorted start
+      "1password"
+      "amazon-photos"
+      "cloudflare-warp"
+      "cryptomator"
+      "discord"
+      "domzilla-caffeine"
+      "elecom-mouse-util"
+      "font-biz-udgothic"
+      "font-biz-udmincho"
+      "font-biz-udpgothic"
+      "font-biz-udpmincho"
+      "google-chrome"
+      "google-drive"
+      "karabiner-elements"
+      "ollama-app"
+      "orion"
+      "raycast"
+      "slack"
+      "tailscale-app"
+      "visual-studio-code"
+      "zoom"
+      # keep-sorted end
+    ];
+    masApps = {
+      # keep-sorted start
+      "辞書 by 物書堂" = 1380563956;
+      DaisyDisk = 411643860;
+      GoodLinks = 1474335294;
+      LINE = 539883307;
+      Pastebot = 1179623856;
+      Things = 904280696;
+      # keep-sorted end
+    };
+    onActivation = {
+      cleanup = "zap";
+    };
+  };
+}
