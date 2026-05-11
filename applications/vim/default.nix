5c4002b2ae41238190e5325915011e8d9793b8b4
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-05-12 08:51:51 +0900

    feat(vim): exec with lsp

--- applications/vim/default.nix
+++ applications/vim/default.nix
@@ -7,14 +7,29 @@
 let
   sources = pkgs.callPackage ./_sources/generated.nix { };
   config = pkgs.callPackage ./config.nix { };
+  vim' = pkgs.vim.overrideAttrs {
+    inherit (sources.vim) src;
+    version = "dev-${sources.vim.date}-${lib.substring 0 8 sources.vim.version}";
+  };
+  vim-with-deps = pkgs.writeShellApplication {
+    name = "vim";
+    runtimeInputs = with pkgs; [
+      vim'
+
+      # keep-sorted start
+      copilot-language-server
+      gopls
+      # keep-sorted end
+    ];
+    text = ''
+      exec vim "$@"
+    '';
+  };
 in
 {
   config = {
-    home.packages = with pkgs; [
-      (pkgs.vim.overrideAttrs {
-        inherit (sources.vim) src;
-        version = "dev-${sources.vim.date}";
-      })
+    home.packages = [
+      vim-with-deps
     ];
     xdg = {
       configFile = {
