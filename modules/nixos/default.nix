26134a3c7afcd5a0259f1418e2b5bac4d52140cc
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-05-15 05:55:30 +0900

    fix: use nixos home-manager module instead of darwin one at nixos

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -6,7 +6,7 @@ in
   imports = [
     # keep-sorted start
     inputs.disko.nixosModules.disko
-    inputs.home-manager.darwinModules.home-manager
+    inputs.home-manager.nixosModules.home-manager
     inputs.sops-nix.nixosModules.sops
     # keep-sorted end
   ];
