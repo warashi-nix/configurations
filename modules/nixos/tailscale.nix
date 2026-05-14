ae4d8c21c40c88fd02ef9f665ed2b37e6df83fda
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-05-15 06:12:24 +0900

    fix: tailscale config

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -11,30 +11,28 @@ let
   cfg = config.warashi.nixos;
 in
 {
-  config =
-    mkIf cfg.enable
-    && cfg.enableTailscale {
-      services.tailscale = {
-        enable = true;
-        useRoutingFeatures = "both";
-        extraUpFlags = [
-          "--ssh"
-          "--accept-routes"
-        ];
-        authKeyFile = config.sops.secrets.tailscale-authkey.path;
-      };
-      networking = {
-        firewall = {
-          trustedInterfaces = [ "tailscale0" ];
-          allowedUDPPorts = [ config.services.tailscale.port ];
-        };
-        nameservers = [
-          "100.100.100.100"
-          "8.8.8.8"
-        ];
-        search = [ "taileef3.ts.net" ];
-      };
-
-      sops.secrets.tailscale-authkey = { };
+  config = mkIf (cfg.enable && cfg.enableTailscale) {
+    services.tailscale = {
+      enable = true;
+      useRoutingFeatures = "both";
+      extraUpFlags = [
+        "--ssh"
+        "--accept-routes"
+      ];
+      authKeyFile = config.sops.secrets.tailscale-authkey.path;
     };
+    networking = {
+      firewall = {
+        trustedInterfaces = [ "tailscale0" ];
+        allowedUDPPorts = [ config.services.tailscale.port ];
+      };
+      nameservers = [
+        "100.100.100.100"
+        "8.8.8.8"
+      ];
+      search = [ "taileef3.ts.net" ];
+    };
+
+    sops.secrets.tailscale-authkey = { };
+  };
 }
