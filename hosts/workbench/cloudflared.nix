4c688c69d75d529b81f668d44d502e4e5fa8bc0b
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-05-23 16:38:31 +0900

    feat: cloudflared

---
+++ hosts/workbench/cloudflared.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+{
+  sops.secrets.cloudflared-creds = {
+  };
+
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
+      "393c3895-c017-4ae2-9eb9-dc0c21fa1e54" = {
+        credentialsFile = config.sops.secrets.cloudflared-creds.path;
+        default = "http_status:404";
+      };
+    };
+  };
+}
