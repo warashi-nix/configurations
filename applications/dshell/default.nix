762f33380f5447a46c80d8ee72b0aa8306d2c94e
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-05-30 16:08:14 +0900

    feat: dshell configs

--- applications/dshell/default.nix
+++ applications/dshell/default.nix
@@ -1,16 +1,50 @@
 {
-  config,
   inputs,
+  config,
+  lib,
   pkgs,
   ...
 }:
+with lib;
 let
+  cfg = config.warashi.dshell;
   dshell = pkgs.callPackage ./package.nix { };
 in
 {
-  home = {
-    packages = [
-      dshell
-    ];
+  options.warashi.dshell = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable warashi dshell program.";
+    };
+    defaultConfig = mkOption {
+      type = types.path;
+      default = ./config/default/.devcontainer;
+      description = "A path that contains devcontainer.json and devcontainer-lock.json";
+    };
+    overrideConfig = mkOption {
+      type = types.path;
+      default = ./config/override/.devcontainer;
+      description = "A path that contains devcontainer.json and devcontainer-lock.json";
+    };
+  };
+  config = mkIf cfg.enable {
+    home = {
+      packages = [
+        dshell
+      ];
+    };
+    xdg = {
+      configFile = {
+        "dshell/default" = {
+          source = cfg.defaultConfig;
+          recursive = true;
+        };
+        "dshell/override" = {
+          source = cfg.overrideConfig;
+          recursive = true;
+        };
+      };
+    };
   };
 }
