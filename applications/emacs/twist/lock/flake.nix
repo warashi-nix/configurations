c56e685ed0f34bc72bea920b84fef942976f1ffd
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-06-06 19:00:08 +0900

    feat(emacs): basic setup and magit

--- applications/emacs/twist/lock/flake.nix
+++ applications/emacs/twist/lock/flake.nix
@@ -1,10 +1,52 @@
 {
   description = "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
   inputs = {
+    compat = {
+      flake = false;
+      owner = "emacs-compat";
+      repo = "compat";
+      type = "github";
+    };
+    cond-let = {
+      flake = false;
+      owner = "tarsius";
+      repo = "cond-let";
+      type = "github";
+    };
+    llama = {
+      flake = false;
+      owner = "tarsius";
+      repo = "llama";
+      type = "github";
+    };
+    magit = {
+      flake = false;
+      owner = "magit";
+      repo = "magit";
+      type = "github";
+    };
+    magit-section = {
+      flake = false;
+      owner = "magit";
+      repo = "magit";
+      type = "github";
+    };
     setup = {
       flake = false;
       type = "git";
       url = "https://codeberg.org/pkal/setup.el";
+    };
+    transient = {
+      flake = false;
+      owner = "magit";
+      repo = "transient";
+      type = "github";
+    };
+    with-editor = {
+      flake = false;
+      owner = "magit";
+      repo = "with-editor";
+      type = "github";
     };
   };
   outputs = { ... }: { };
