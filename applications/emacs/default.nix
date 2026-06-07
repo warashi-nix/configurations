7336402f8827085fca9d43e6726d5fa00122f77f
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-06-07 14:46:02 +0900

    feat(emacs): service integration and truecolor support

--- applications/emacs/default.nix
+++ applications/emacs/default.nix
@@ -8,8 +8,21 @@ in
 
     enable = true;
     emacsclient.enable = true;
+    serviceIntegration.enable = true;
     createInitFile = true;
     createManifestFile = true;
     config = inputs.my-emacs.packages.${system}.default;
+  };
+  launchd.agents.emacs.config = {
+    EnvironmentVariables = {
+      COLORTERM = "truecolor";
+    };
+  };
+  systemd.user.services.emacs = {
+    Service = {
+      Environment = [
+        "COLORTERM=truecolor"
+      ];
+    };
   };
 }
