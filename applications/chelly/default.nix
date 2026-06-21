a4bd51e36ad9d524b94fd12ade8412dc1b184114
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-06-21 11:14:49 +0900

    feat: add chelly settings

---
+++ applications/chelly/default.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib;
+let
+  cfg = config.warashi.chelly;
+in
+{
+  options.warashi.chelly = {
+    enable = mkOption {
+      type = types.bool;
+      description = "Enable Chelly options.";
+      default = true;
+    };
+    dockerFile = mkOption {
+      type = types.path;
+      description = "Dockerfile for Chelly.";
+      default = ./Dockerfile;
+    };
+    settings = mkOption {
+      type = types.attrs;
+      description = "Settings for Chelly.";
+      default = {
+        additional_mounts = [
+          "chelly-nix:/nix"
+          "${config.home.homeDirectory}/.copilot:/home/warashi/.copilot"
+        ];
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    xdg.configFile = {
+      "chelly/config.toml".source = (pkgs.formats.toml { }).generate "chelly-config.toml" cfg.settings;
+      "chelly/Dockerfile".source = cfg.dockerFile;
+    };
+  };
+}
